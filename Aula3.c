f26f4000fcfdd174daeaf94bc50fdbaa40d1291f
Josias Gabriel da Silva <josiasjgs2003@gmail.com>
2023-04-28 11:33:27 -0700

    Tipos Básicos (int, float, char, etc.) Ex1

---
+++ Aula3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Tamanho do armazenamento do tipo int: %d \n", sizeof(int));
+    printf("Tamanho do armazenamento do tipo short: %d \n", sizeof(short));
+    printf("Tamanho do armazenamento do tipo long: %d \n", sizeof(long));
+
+    return 0;
+}
