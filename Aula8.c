2e4858209c95de8ab1b1e68f137d0e80d3d45f5b
Josias Gabriel da Silva <josiasjgs2003@gmail.com>
2023-04-29 14:19:23 -0700

    Função printf - Sequências de Escape

---
+++ Aula8.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Aspas Simples: \' \n");
+    printf("Aspas Duplas: \" \n");
+    printf("Interrogação: \? \n");
+    printf("Barra invertida: \\ \n");
+    printf("Mudar de Linha: \n \n");
+    printf("Tab: \t Tabulado \n");
+    printf("Cursor no Final da Linha: \r \n");
+    printf("Sinal Sonoro: \a");
+
+    return 0;
+}
